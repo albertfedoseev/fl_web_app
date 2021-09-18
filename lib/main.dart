a5c9f38d41496c7ff8bdcde851790fc5bc20bf63
Albert <fedoseevalbert@gmail.com>
2021-09-18 13:43:44 +0300

    test13:43

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 35;
+  int _counter = 88;
 
   void _incrementCounter() {
     setState(() {
